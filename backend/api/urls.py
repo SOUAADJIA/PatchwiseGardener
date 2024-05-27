8a5013a641b30eb290ceafec7ce674c98981059a
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-05-27 16:49:39 +0100

    setup V2 done: before external API added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("plants/delete/<int:pk>/", views.PlantDelete.as_view(), name="delete-plant"),
     path("posts/", views.PostListCreate.as_view(), name="post-list"),
     path("posts/<int:pk>/", views.PostRetrieveUpdateDestroy.as_view(), name="post-detail"),
+    path("posts/delete/<int:pk>/", views.PostRetrieveUpdateDestroy.as_view(), name="delete-post"),
     path("posts/<int:post_id>/comments/", views.CommentListCreate.as_view(), name="comment-list"),
     path("comments/<int:pk>/", views.CommentRetrieveUpdateDestroy.as_view(), name="comment-detail"),
 ]
