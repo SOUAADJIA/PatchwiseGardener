b816ee89b6aaeca3a9c5ba53063e286a24f24bf4
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-06-03 02:19:46 +0100

    Update page Plants

--- frontend/src/pages/Plants.jsx
+++ frontend/src/pages/Plants.jsx
@@ -7,8 +7,8 @@ function Plants() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 30; // Change this value as needed
-  const totalPages = Math.ceil(3000 / itemsPerPage); // Total number of pages
+  const itemsPerPage = 100;
+  const totalPages = Math.ceil(3000 / itemsPerPage);
 
   useEffect(() => {
     const fetchPlants = async () => {
@@ -30,6 +30,16 @@ function Plants() {
     setCurrentPage(page);
   };
 
+  const handleDetailsClick = async (id) => {
+    try {
+      const response = await api.get(`/api/species/${id}/`);
+      // Display plant details (e.g., in a modal or a separate page)
+      console.log("Plant Details:", response.data);
+    } catch (error) {
+      console.error("Error fetching plant details:", error);
+    }
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -47,18 +57,30 @@ function Plants() {
             <h2>{plant.common_name}</h2>
             {plant.scientific_name && <p><i>{plant.scientific_name.join(", ")}</i></p>}
             {plant.default_image && <img src={plant.default_image.thumbnail} alt={plant.common_name} />}
-            {/* Display other plant information */}
+            <button onClick={() => handleDetailsClick(plant.id)}>Details</button>
           </div>
         ))}
       </div>
       <div className="pagination">
-        {[...Array(totalPages).keys()].map((page) => (
-          <span key={page} className={`page-item ${currentPage === page + 1 ? 'active' : ''}`}>
-            <button className="page-link" onClick={() => handlePageChange(page + 1)}>
-              {page + 1}
-            </button>
-          </span>
-        ))}
+        {[...Array(totalPages).keys()].map((page) => {
+          const firstPage = 1;
+          const lastPage = totalPages;
+          const showFirstPages = currentPage <= 3;
+          const showLastPages = currentPage >= totalPages - 2;
+          const showPage = showFirstPages || showLastPages || (page >= currentPage - 1 && page <= currentPage + 1);
+          
+          if (!showPage && !(page === firstPage || page === lastPage)) {
+            return null;
+          }
+
+          return (
+            <span key={page} className={`page-item ${currentPage === page + 1 ? 'active' : ''}`}>
+              <button className="page-link" onClick={() => handlePageChange(page + 1)}>
+                {page + 1}
+              </button>
+            </span>
+          );
+        })}
       </div>
     </div>
   );
